f29cb38583a3d8212a84914694be0d1a66202d2a
webstrdy00 <les_roka22@naver.com>
2025-01-18 10:37:37 +0900

    [add] init commit

---
+++ src/main/java/com/example/onboardingassignment/OnboardingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.onboardingassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingAssignmentApplication.class, args);
+	}
+
+}
